adc0ecd9daacd1430533d9bb9c2898f54c202560
Rakshitha-th <rakshiirakk@gmail.com>
2026-05-18 12:10:35 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,296 +1,157 @@
-import customtkinter as ctk
-from PIL import Image, ImageTk
-import cv2
+from flask import Flask, render_template, Response, jsonify
+import cv2, os, joblib
 import numpy as np
-import joblib
-import os
-import tkinter as tk
 
 from datetime import datetime
 from insightface.app import FaceAnalysis
 from sklearn.metrics.pairwise import cosine_similarity
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("dark-blue")
-app = ctk.CTk()
 
-app.title("AI Face Recognition Attendance System")
-app.state("zoomed")
-
-app.configure(fg_color="#0f172a")
-
+app = Flask(__name__)
 
 known_embeddings = joblib.load("models/embeddings.pkl")
 known_names = joblib.load("models/names.pkl")
 normalizer = joblib.load("models/normalizer.pkl")
 
-
 face_app = FaceAnalysis(name='buffalo_l')
-
-
 face_app.prepare(ctx_id=-1)
 
 os.makedirs("attendance", exist_ok=True)
-
-date_today = datetime.now().strftime("%Y-%m-%d")
-
-attendance_file = f"attendance/attendance_{date_today}.csv"
-
-# create attendance file
+attendance_file = "attendance/attendance.csv"
 if not os.path.exists(attendance_file):
 
     with open(attendance_file, "w") as f:
-        f.write("Name,Time\n")
+
+        f.write("Name,Date,Time\n")
 
 marked_names = set()
 
-# LEFT SIDEBAR
-left_frame = ctk.CTkFrame(app,width=260,corner_radius=20,fg_color="#111827"
-)
+face_count = 0
 
-left_frame.pack(side="left",fill="y",padx=15,pady=15
-)
-
-# CENTER FRAME
-center_frame = ctk.CTkFrame(app,corner_radius=20,fg_color="#111827"
-)
-
-center_frame.pack(side="left",fill="both",expand=True,padx=10,pady=15
-)
-
-# RIGHT FRAME
-right_frame = ctk.CTkFrame(app,width=320,corner_radius=20,fg_color="#111827"
-)
-
-right_frame.pack(side="right",fill="y",padx=15,pady=15
-)
-
-logo = ctk.CTkLabel(left_frame,text="FaceLog AI",font=("Arial", 34, "bold"),text_color="#60a5fa"
-)
-
-logo.pack(pady=(25, 5))
-
-subtitle = ctk.CTkLabel(left_frame,text="FACE RECOGNITION\nATTENDANCE SYSTEM",font=("Arial", 14),text_color="gray"
-)
-
-subtitle.pack(pady=(0, 25))
-
-clock_label = ctk.CTkLabel(left_frame,text="",font=("Arial", 20, "bold"),text_color="white"
-)
-
-clock_label.pack(pady=15)
-
-def update_clock():
-
-    now = datetime.now().strftime("%I:%M:%S %p\n%d-%m-%Y")
-    clock_label.configure(text=now)
-
-    clock_label.after(1000, update_clock)
-
-update_clock()
-
-
-registered_card = ctk.CTkFrame(left_frame,fg_color="#1e293b",corner_radius=18
-)
-
-registered_card.pack(padx=15,pady=10,fill="x"
-)
-
-registered_count = len(known_names)
-
-registered_label = ctk.CTkLabel(
-    registered_card,
-    text=f"{registered_count}\nREGISTERED",
-    font=("Arial", 26, "bold"),
-    text_color="white"
-)
-
-registered_label.pack(pady=25)
-
-present_card = ctk.CTkFrame(
-    left_frame,fg_color="#1e293b",corner_radius=18
-)
-
-present_card.pack(padx=15,pady=10,fill="x"
-)
-
-present_label = ctk.CTkLabel(present_card,text="0\nPRESENT TODAY",font=("Arial", 26, "bold"),text_color="white"
-)
-
-present_label.pack(pady=25)
-
-# FACE COUNT CARD
-
-face_card = ctk.CTkFrame(left_frame,fg_color="#1e293b",corner_radius=18
-)
-
-face_card.pack(padx=15,pady=10,fill="x"
-)
-
-face_count_label = ctk.CTkLabel(face_card,text="0\nFACES DETECTED",font=("Arial", 24, "bold"),text_color="white"
-)
-
-face_count_label.pack(pady=25)
-
-# CAMERA STATUS
-
-status_label = ctk.CTkLabel(left_frame,text="● CAMERA ACTIVE",font=("Arial", 16, "bold"),text_color="#22c55e"
-)
-
-status_label.pack(pady=25)
-
-def open_attendance():
-
-    os.startfile("attendance")
-
-attendance_btn = ctk.CTkButton(left_frame,text="Open Attendance",height=45,corner_radius=12,font=("Arial", 16, "bold"),fg_color="#2563eb",hover_color="#1d4ed8",
-    command=open_attendance
-)
-
-attendance_btn.pack(padx=20,pady=(20, 10),fill="x"
-)
-
-# EXIT BUTTON
-
-def close_app():
-    cap.release()
-    app.destroy()
-exit_btn = ctk.CTkButton(left_frame,text="Exit",height=45,corner_radius=12,font=("Arial", 16, "bold"),fg_color="#dc2626",hover_color="#b91c1c",command=close_app
-)
-
-exit_btn.pack(padx=20,pady=10,fill="x"
-)
-
-# CENTER PANEL
-
-camera_title = ctk.CTkLabel(
-    center_frame,
-    text="LIVE FACE RECOGNITION",
-    font=("Arial", 28, "bold"),
-    text_color="white"
-)
-
-camera_title.pack(pady=15)
-camera_label = ctk.CTkLabel(
-    center_frame,
-    text=""
-)
-camera_label.pack(pady=10)
-
-# RIGHT PANEL
-
-logs_title = ctk.CTkLabel(
-    right_frame,
-    text="Today's Attendance",
-    font=("Arial", 28, "bold"),
-    text_color="white"
-)
-
-logs_title.pack(pady=20)
-
-log_box = ctk.CTkTextbox(right_frame,width=280,height=750,corner_radius=15,fg_color="#0f172a",font=("Consolas", 15),text_color="white"
-)
-
-log_box.pack(
-    padx=15,
-    pady=10
-)
 cap = cv2.VideoCapture(0)
-def refresh_present_count():
-    count = len(marked_names)
-    present_label.configure(
-        text=f"{count}\nPRESENT TODAY"
-    )
 
-def update_camera():
-    ret, frame = cap.read()
+def generate_frames():
+    global face_count
 
-    if ret:
+    while True:
+        success, frame = cap.read()
+
+        if not success:
+            break
 
         faces = face_app.get(frame)
-        # face count
-        face_count_label.configure(
-            text=f"{len(faces)}\nFACES DETECTED"
-        )
+        face_count = len(faces)
 
         for face in faces:
-
             x1, y1, x2, y2 = map(int, face.bbox)
-            embedding = face.embedding.reshape(1, -1)
-            embedding = normalizer.transform(embedding)
+            embedding = normalizer.transform(
+                [face.embedding]
+            )
+
             similarities = cosine_similarity(
                 embedding,
                 known_embeddings
             )[0]
 
-            best_index = np.argmax(similarities)
-            best_similarity = similarities[best_index]
+            idx = np.argmax(similarities)
+            similarity = similarities[idx]
+            name = known_names[idx]
 
-            name = known_names[best_index]
-            if best_similarity < 0.65:
+            if similarity < 0.70:
                 name = "Unknown"
-
                 color = (0, 0, 255)
-
-                cv2.putText(
-                    frame,
-                    "Unknown",
-                    (x1, y2 + 30),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.8,
-                    (0, 0, 255),
-                    2
-                )
 
             else:
 
-                color = (0, 255, 120)
+                color = (0, 255, 0)
                 if name not in marked_names:
                     marked_names.add(name)
-                    refresh_present_count()
-                    current_time = datetime.now().strftime("%H:%M:%S")
-
-                    # LOG BOX
-
-                    log_box.insert(
-                        "end",
-                        f"{name:<15} {current_time}\n"
+                    current_date = datetime.now().strftime(
+                        "%d-%m-%Y"
                     )
-                    log_box.see("end")
 
-                    # SAVE CSV
+                    current_time = datetime.now().strftime(
+                        "%H:%M:%S"
+                    )
+
                     with open(attendance_file, "a") as f:
+
                         f.write(
-                            f"{name},{current_time}\n"
+                            f"{name},{current_date},{current_time}\n"
                         )
-            cv2.rectangle(
-                frame,
-                (x1, y1),
-                (x2, y2),
-                color,
-                3
+
+            cv2.rectangle(frame,(x1, y1),(x2, y2),color,3
             )
 
-            text = f"{name} ({best_similarity:.2f})"
-            cv2.putText(
-                frame,
-                text,
-                (x1, y1 - 10),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.7,
-                color,
-                2
+            cv2.putText(frame,f"{name} ({similarity:.2f})",(x1, y1 - 10),cv2.FONT_HERSHEY_SIMPLEX,0.8,color,2
             )
-        frame = cv2.cvtColor(
-            frame,
-            cv2.COLOR_BGR2RGB
+
+        ret, buffer = cv2.imencode('.jpg',frame
         )
 
-        img = Image.fromarray(frame)
-        img = img.resize((950, 700))
-        imgtk = ImageTk.PhotoImage(image=img)
-        camera_label.imgtk = imgtk
-        camera_label.configure(image=imgtk)
+        frame = buffer.tobytes()
 
-    camera_label.after(10, update_camera)
-update_camera()
-app.mainloop()
+        yield (
+            b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' +
+            frame +
+            b'\r\n'
+        )
+
+@app.route('/')
+
+def index():
+    attendance = []
+    if os.path.exists(attendance_file):
+
+        with open(attendance_file, "r") as f:
+            lines = f.readlines()[1:]
+            for line in lines:
+                row = line.strip().split(",")
+                if len(row) == 3:
+                    attendance.append(row)
+
+    return render_template(
+
+        "index.html",
+        registered_count=len(known_names),
+        face_count=face_count,
+        attendance=attendance[::-1]
+
+    )
+
+@app.route('/data')
+
+def data():
+    attendance = []
+    if os.path.exists(attendance_file):
+        with open(attendance_file, "r") as f:
+            lines = f.readlines()[1:]
+            for line in lines:
+                row = line.strip().split(",")
+                if len(row) == 3:
+
+                    attendance.append(row)
+
+    return jsonify({
+
+        "face_count": face_count,
+        "attendance": attendance[::-1]
+
+    })
+
+@app.route('/video')
+
+def video():
+    return Response(
+        generate_frames(),
+        mimetype='multipart/x-mixed-replace; boundary=frame'
+
+    )
+
+if __name__ == "__main__":
+
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
--- train.py
+++ train.py
@@ -1,71 +1,151 @@
-import os
-import cv2
+import os, cv2, random, joblib
 import numpy as np
-import joblib
 
-from collections import defaultdict
 from insightface.app import FaceAnalysis
 from sklearn.preprocessing import Normalizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import (accuracy_score,precision_score,recall_score,f1_score,confusion_matrix)
+
+DATASET = "Dataset"
+MODEL_DIR = "models"
+
+THRESHOLD = 0.70
+
+random.seed(42)
+np.random.seed(42)
 
 app = FaceAnalysis(name='buffalo_l')
-app.prepare(ctx_id=0)
+app.prepare(ctx_id=-1)
 
+def augment(img):
 
-dataset_path = "Dataset"
+    return [
+        img,
+        cv2.flip(img, 1)
+    ]
 
-# store embeddings person-wise
-person_embeddings = defaultdict(list)
-people = os.listdir(dataset_path)
+train = []
+val = []
+test = []
 
-for person in people:
-    person_path = os.path.join(dataset_path, person)
+for person in os.listdir(DATASET):
 
-    if not os.path.isdir(person_path):
+    path = os.path.join(DATASET, person)
+    if not os.path.isdir(path):
         continue
 
-    for image_name in os.listdir(person_path):
+    images = [
+        (os.path.join(path, img), person)
+        for img in os.listdir(path)
+    ]
 
-        image_path = os.path.join(person_path, image_name)
+    random.shuffle(images)
+    n = len(images)
+    train += images[:int(0.6*n)]
+    val += images[
+        int(0.6*n):int(0.8*n)
+    ]
 
-        img = cv2.imread(image_path)
+    test += images[int(0.8*n):]
 
-        if img is None:
-            continue
+print(f"\nTrain:{len(train)}")
+print(f"Validation:{len(val)}")
+print(f"Test:{len(test)}")
+
+person_embeddings = {}
+
+print("\nExtracting embeddings...")
+for img_path, person in train:
+    img = cv2.imread(img_path)
+    if img is None:
+        continue
+
+    for aug in augment(img):
 
         try:
-
-            faces = app.get(img)
-
+            faces = app.get(aug)
             if len(faces) == 0:
                 continue
 
-            embedding = faces[0].embedding
+            face = max(faces,key=lambda x:(x.bbox[2]-x.bbox[0]) *(x.bbox[3]-x.bbox[1]))
 
-            person_embeddings[person].append(embedding)
+            embedding = face.embedding
+            if person not in person_embeddings:\
+                person_embeddings[person] = []
+            person_embeddings[person].append(
+                embedding
+            )
+        except Exception as e:
+            print(e)
 
-            print(f"Processed: {person} - {image_name}")
-
-        except:
-            continue
 known_embeddings = []
 known_names = []
 
 for person, embeds in person_embeddings.items():
+    mean_embedding = np.mean(embeds,axis=0)
 
-    mean_embedding = np.mean(embeds, axis=0)
     known_embeddings.append(mean_embedding)
     known_names.append(person)
 
-
-known_embeddings = np.array(known_embeddings)
 normalizer = Normalizer(norm='l2')
-known_embeddings = normalizer.transform(known_embeddings)
+known_embeddings = normalizer.fit_transform(np.array(known_embeddings))
 
-os.makedirs("models", exist_ok=True)
+os.makedirs(MODEL_DIR, exist_ok=True)
 
-joblib.dump(known_embeddings, "models/embeddings.pkl")
-joblib.dump(known_names, "models/names.pkl")
-joblib.dump(normalizer, "models/normalizer.pkl")
+joblib.dump(known_embeddings,f"{MODEL_DIR}/embeddings.pkl")
+joblib.dump(known_names,f"{MODEL_DIR}/names.pkl")
+joblib.dump(normalizer,f"{MODEL_DIR}/normalizer.pkl")
 
-print("\nEmbeddings Saved Successfully")
-print("Total Persons:", len(known_names))
+print("\nModels Saved Successfully")
+
+def evaluate(data, name):
+
+    y_true = []
+    y_pred = []
+    for img_path, actual in data:
+        img = cv2.imread(img_path)
+        if img is None:
+            continue
+        try:
+            faces = app.get(img)
+            if len(faces) == 0:
+                continue
+
+            face = max(faces,key=lambda x:(x.bbox[2]-x.bbox[0]) *(x.bbox[3]-x.bbox[1]))
+            embedding = normalizer.transform([face.embedding])
+            similarities = cosine_similarity(embedding,known_embeddings)[0]
+
+            best_index = np.argmax(similarities)
+            best_similarity = similarities[best_index]
+            prediction = (
+                known_names[best_index]
+                if best_similarity > THRESHOLD
+                else "Unknown"
+            )
+
+            y_true.append(actual)
+            y_pred.append(prediction)
+
+        except Exception as e:
+            print(e)
+
+    if len(y_true) == 0:
+        print("\nNo Predictions Made")
+        return
+
+    print(f"\n{name} Results")
+    print("-" * 35)
+
+    print(f"Accuracy : {accuracy_score(y_true,y_pred)*100:.2f}%")
+    print(f"Precision: {precision_score(y_true,y_pred,average='weighted',zero_division=0)*100:.2f}%")
+    print(f"Recall   : {recall_score(y_true,y_pred,average='weighted',zero_division=0)*100:.2f}%")
+    print(f"F1 Score : {f1_score(y_true,y_pred,average='weighted',zero_division=0)*100:.2f}%")
+    print("\nConfusion Matrix")
+    print("-" * 35)
+
+    cm = confusion_matrix(y_true,y_pred)
+    print(cm)
+
+evaluate(val,"Validation")
+evaluate(test,"Test")
+print("\nTraining Completed Successfully")
